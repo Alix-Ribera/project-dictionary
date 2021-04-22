5eef8d9c147b727b6e44f70ff803dfdbb155f3f3
Alix Simo Ribera <alixsimoribera@gmail.com>
2021-04-23 00:58:29 +0200

    Basic form and jsx input created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-
+import DictionarySearch from "./DictionarySearch";
 export default function App() {
   return (
     <div className="App">
-      <header></header>
+      <div className="container">
+        <header>Dictionary app</header>
+        <main>
+          <DictionarySearch />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Alix Simo</small>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/DictionarySearch.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./DictionarySearch.css";
+export default function DictionarySearch() {
+  let [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for "${keyword}" definition`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="DictionarySearch">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
